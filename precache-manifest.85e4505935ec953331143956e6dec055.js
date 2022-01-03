a4a83fac2a7982565a99d7c2780b811ec2f3af4b
Artem Makarchenko <artemmakar.dev@gmail.com>
2022-01-03 15:22:59 +0200

    Updates

---
+++ precache-manifest.85e4505935ec953331143956e6dec055.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50af4dfd25eeda3ef050",
+    "url": "/css/app.fe1588a6.css"
+  },
+  {
+    "revision": "6c63e15be597b1d41e8a",
+    "url": "/css/chunk-vendors.a15bb5c7.css"
+  },
+  {
+    "revision": "d022dbc7d80377a638a732b00cee9b12",
+    "url": "/img/book.d022dbc7.jpeg"
+  },
+  {
+    "revision": "ca194025b127a282bd0421aad2889843",
+    "url": "/img/dream.ca194025.jpeg"
+  },
+  {
+    "revision": "a9a9534810cf42c70af0b34006c45b49",
+    "url": "/img/lion.a9a95348.jpeg"
+  },
+  {
+    "revision": "af9d6e6b9f174fe72c93577ac48ab0cd",
+    "url": "/img/mushroom.af9d6e6b.jpeg"
+  },
+  {
+    "revision": "2301cfb57f06d61f46fc8ecf559de75f",
+    "url": "/img/sun.2301cfb5.jpeg"
+  },
+  {
+    "revision": "b665af711e84efdaad70872ebf9489a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50af4dfd25eeda3ef050",
+    "url": "/js/app.1571f9cb.js"
+  },
+  {
+    "revision": "6c63e15be597b1d41e8a",
+    "url": "/js/chunk-vendors.571a1c60.js"
+  },
+  {
+    "revision": "1d448f52f19358b1cad35db15cab6357",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
